298fcf90fd11553732f2e932076b373cf174ad0e
Sumit Jaiswal <sumitjaiswal7870@gmail.com>
2026-07-20 21:22:57 +0530

    Fix: clean up compiled JS files from git tracking in src folder and enable deleteOutDir

--- flutter-nest-househelp-master/src/system-status/dto/system-readiness.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SystemReadinessDto = void 0;
-var SystemReadinessDto = /** @class */ (function () {
-    function SystemReadinessDto() {
-    }
-    return SystemReadinessDto;
-}());
-exports.SystemReadinessDto = SystemReadinessDto;
